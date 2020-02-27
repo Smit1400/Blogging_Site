62a3daa9ce7cc3e56478cd31e9de34bb38a3e39e
Smit1400 <46996928+Smit1400@users.noreply.github.com>
2020-02-27 16:58:46 +0530

    paginated

---
+++ Blogging_Site/users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+# Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	image = models.ImageField(default='default.png', upload_to='profile_pics')
+
+	def __str__(self):
+		return f'{self.user.username} Profile' 
+
+	def save(self):
+		super().save()
+
+		img = Image.open(self.image.path)
+		if img.height>300 or img.width>300:
+			out_size=(300,300)
+			img.thumbnail(out_size)
+			img.save(self.image.path)
+
